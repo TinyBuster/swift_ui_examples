4b4fc380cb2d5d30f2f2d6d6263c8536b3228ffa
bai <baijiawen.sw@gmail.com>
2021-02-10 22:45:03 -0500

    Initial Commit

---
+++ animated_text/animated_textApp.swift
@@ -0,0 +1,17 @@
+//
+//  animated_textApp.swift
+//  animated_text
+//
+//  Created by Jiawen Bai on 2/10/21.
+//
+
+import SwiftUI
+
+@main
+struct animated_textApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
